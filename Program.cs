9c6955fd618c209f611ed93eb767149a8dbfe4c7
Guestik <lukas.andel@seznam.cz>
2016-05-01 20:28:05 +0200

    Added files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿//whoami command - Lukáš Anděl
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace whoami
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Environment.UserName); //Vypíše jméno uživatele
+        }
+    }
+}
