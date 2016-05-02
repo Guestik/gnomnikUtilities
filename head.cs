cb34625d5021e7480c5399c70d62b76c841252b1
Guestik <lukas.andel@seznam.cz>
2016-05-02 15:04:39 +0200

    Update head.cs

--- head.cs
+++ head.cs
@@ -1,4 +1,18 @@
 ﻿//head utility - Lukáš Anděl
+﻿//This file is part of Gnomnik.
+
+//    Gnomnik is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+
+//    Gnomnik is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+
+//    You should have received a copy of the GNU General Public License
+//    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
 using System;
 using System.Collections.Generic;
 using System.Linq;
